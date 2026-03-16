e2943582229d42b81c3a67a2d208d91bb7c7d42f
Vicente Cartas <vicente.cartas@outlook.com>
2026-03-15 21:36:01 -0700

    Update to 1.0.1

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -3,7 +3,7 @@
  */
 
 /** Current application version */
-export const APP_VERSION = '1.0.0';
+export const APP_VERSION = '1.0.1';
 
 /** Application name */
 export const APP_NAME = 'Alternity Warship Generator';
